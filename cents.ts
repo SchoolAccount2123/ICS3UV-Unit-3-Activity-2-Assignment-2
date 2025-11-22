89de199f60e5bfa6335aaf590e00c7a5756c24eb
Connor-Wilkins <159037610+SchoolAccount2123@users.noreply.github.com>
2025-11-22 16:51:25 +0000

    finished cents code

--- cents.ts
+++ cents.ts
@@ -1,15 +1,24 @@
 // Author: Connor Wilkins
 // Version: 1.0.0
 // Date: 2025-11-21
-// Fileoverview:
+// Fileoverview: This program will take the users 
+// input and read the number in cents
 
 
+// variables
+let centasstring: string;
+let centasnum: number;
 
-function main() {
-  //test
-  
+//getting the input
+centasstring = prompt("Input the number of cents please:") || "no number entered!";
 
-  
+centasnum = parseInt(centasstring)
 
 
-}
+//calculating the dollars and cents using integer arithmeitc
+const dollars: number = Math.floor(centasnum / 100);
+const remaningcents: number = centasnum % 100;
+
+//displaying the results
+console.log("The cents you inputted were", centasnum)
+console.log("That is", dollars, "dollars and", remaningcents, "cents.");
